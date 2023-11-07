069cd7b7a201f1ed0567d934792277828186f59f
Anik Das <onikd08@gmail.com>
2023-11-07 09:42:09 +0200

    Delete useDataLoader hook as we are loading data using react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import CategoryDetails from "./components/CategoryDetails/CategoryDetails.jsx";
+import MealDetails from "./components/MealDetails/MealDetails.jsx";
+import Categories from "./components/Categories/Categories.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Categories></Categories>,
+    loader: () =>
+      fetch("https://www.themealdb.com/api/json/v1/1/categories.php"),
+  },
+
+  {
+    path: "/category/:name",
+    element: <CategoryDetails></CategoryDetails>,
+    loader: ({ params }) =>
+      fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${params.name}`
+      ),
+  },
+  {
+    path: "/meal/:id",
+    element: <MealDetails></MealDetails>,
+    loader: ({ params }) =>
+      fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`
+      ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
